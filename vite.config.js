2ca2ba955800703236bc9de8be0d76ff8fadf9e7
David G <davidgcalles91@gmail.com>
2024-10-05 03:43:41 +0200

    host config

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
       host: '0.0.0.0', // Listen on all network interfaces
       port: 8080,       // Default port is 3000, change as needed
       // Ensure your firewall allows traffic on this port.
-      hmr: {
-        host: '192.168.1.78', // Replace 'your-local-ip' with the actual IP
-      }
+      //hmr: {
+        //host: '192.168.1.78', // Replace 'your-local-ip' with the actual IP
+      //}
     },
     plugins: [
       vue(),
