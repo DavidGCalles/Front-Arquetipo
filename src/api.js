2a1dc8bb7b9b9d86f9cb8071aa5b0827b9284fc5
DavidGCalles <davidgcalles91@gmail.com>
2025-01-27 20:45:58 +0100

    AR-12 AR-13
    Backend connection and new page to consume demo_crud.
    
    Changes in content to fit purpose

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL : 'https://back-arquetipo-856517455627.europe-southwest1.run.app/api',
-  //baseURL: 'http://localhost:5000/api', // Base URL for your Flask API
+  //baseURL : 'https://back-arquetipo-856517455627.europe-southwest1.run.app/api',
+  baseURL: 'http://localhost:5000/', // Base URL for your Flask API
   headers: {
     'Content-Type': 'application/json',
   },
@@ -12,7 +12,7 @@ const apiClient = axios.create({
 // Function to ping the server
 export const pingServer = async () => {
   try {
-    const response = await apiClient.get('/ping');
+    const response = await apiClient.get('ping');
     return response.data;
   } catch (error) {
     console.error('Error in pingServer:', error);
@@ -34,5 +34,57 @@ export const initiateOAuthLogin = async () => {
   }
 };
 
+// Function to fetch data from the API
+export const fetchData = async () => {
+  try {
+    const response = await apiClient.get('/demo_crud');
+    return response.data;
+  } catch (error) {
+    console.error('Error in fetchData:', error);
+    throw error;
+  }
+};
 
-// You can add more API functions here as needed
+// Function to fetch all items
+export const fetchAllItems = async () => {
+  try {
+    const response = await apiClient.get('/demo_crud');
+    return response.data;
+  } catch (error) {
+    console.error('Error in fetchAllItems:', error);
+    throw error;
+  }
+};
+
+// Function to insert a new item
+export const insertNewItem = async (newItem) => {
+  try {
+    const response = await apiClient.post('/demo_crud', newItem);
+    return response.data;
+  } catch (error) {
+    console.error('Error in insertNewItem:', error);
+    throw error;
+  }
+};
+
+// Function to search items
+export const searchItems = async (queryParams) => {
+  try {
+    const response = await apiClient.get('/demo_crud/search', { params: queryParams });
+    return response.data;
+  } catch (error) {
+    console.error('Error in searchItems:', error);
+    throw error;
+  }
+};
+
+// Function to delete an item by ID
+export const deleteItem = async (itemId) => {
+  try {
+    const response = await apiClient.delete(`/demo_crud/item/${itemId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error in deleteItem:', error);
+    throw error;
+  }
+};
